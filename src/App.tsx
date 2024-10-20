6451e5dc610553bc8f73f8e3ef70078b0236194e
chinmaysah <chinmaysah@gmail.com>
2024-10-20 23:10:07 +0530

    Add Employees component to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Register from './Register';
+import Employees from './Employees';
 import './App.css';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
+      <Route path="/employees" element={<Employees />} />
     </Routes>
   );
 }
